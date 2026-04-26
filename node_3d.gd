b48dcd52d91a75fd6434fcc959716eb096ee5799
Vytautas Butėnas <leekleak@proton.me>
2026-04-26 14:22:03 +0200

    add robot model

--- node_3d.gd
+++ node_3d.gd
@@ -1,6 +1,12 @@
 extends Node3D
 
-func _ready() -> void:
-	$"../Control/HSlider".value = rotation_degrees.y
-func _on_h_slider_value_changed(value: float) -> void:
-	rotation_degrees.y = value
+
+var last_position = 0
+func _process(delta: float) -> void:
+	if (Input.is_action_just_pressed("lmb")):
+		last_position = get_viewport().get_mouse_position().x
+	if (Input.is_action_pressed("lmb")):
+		var new_position = get_viewport().get_mouse_position().x
+		rotation_degrees.y -= (new_position - last_position) / 2
+		last_position = new_position
+	
